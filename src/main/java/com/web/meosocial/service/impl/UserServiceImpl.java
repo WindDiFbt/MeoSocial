7123e73028c1e998159c7ed5630cbfeba8895ac9
Nguyen The Phong <phongghast@gmail.com>
2024-12-28 18:55:59 +0700

    setting application and test controller

---
+++ src/main/java/com/web/meosocial/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.web.meosocial.service.impl;
+
+import com.web.meosocial.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
