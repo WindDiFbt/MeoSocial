9be44123d3d8715704b248834b5257241b89d03f
Nguyen The Phong <phongghast@gmail.com>
2024-12-30 20:52:38 +0700

    changes file

--- src/main/java/com/web/meosocial/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.web.meosocial.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-}
--- src/main/java/com/web/meosocial/service/impl/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.web.meosocial.service.impl;
-
-import com.web.meosocial.service.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
